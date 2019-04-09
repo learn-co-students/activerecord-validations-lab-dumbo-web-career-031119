71c57b99d27a01a89688fa782e5cb9a3fb2be04c
github username <github email address>
2019-04-09 11:26:02 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,11 @@
 # end
 
 class Post < ActiveRecord::Base
-  validates :title,presence:true,length:{minimum:1}
+  # include ActiveModel::Validations
+  validates :title,presence:true
   validates :content,length:{minimum: 250}
   validates :summary,length:{maximum:250}
   validates :category,inclusion: {in: ["Fiction","Non Fiction"]}
-  include ActiveModel::Validations
   validate :check_for_click_bait
 
   def check_for_click_bait
