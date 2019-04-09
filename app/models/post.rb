9d55a9734e7d09fbf47b12bdd84c4420d86dfc52
github username <github email address>
2019-04-09 11:09:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
+# class MyValidator < ActiveModel::EachValidator
+#   def validate(post)
+#     unless post.title.include?"Won't believe"||"Secret"||"Top[0-9]"||"Guess"
+#       post.errors[:title] << "More Click-bait please!"
+#     end
+#   end
+# end
+
 class Post < ActiveRecord::Base
+  validates :title,presence:true,length:{minimum:1}
+  validates :content,length:{minimum: 250}
+  validates :summary,length:{maximum:250}
+  validates :category,inclusion: {in: ["Fiction","Non Fiction"]}
+  include ActiveModel::Validations
+  validate :check_for_click_bait
+
+  def check_for_click_bait
+    if title == nil
+      errors.add(:title, "title can't be nil")
+    elsif title.exclude?("Won't Believe"||"Secret"|| "Top [number]"|| "Guess")
+          errors.add(:title, "title needs more clickbait")
+    else
+   end
+ end
 end
