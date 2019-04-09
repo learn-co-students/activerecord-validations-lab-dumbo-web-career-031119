a9a77afb5873509fe6af08d2427f243ca75034df
tpr1992 <terence.pataneronan@gmail.com>
2019-04-09 18:11:01 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+
+  validates_presence_of :title
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum:250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "Category must be either Fiction or Non-Fiction" }
+  validate :clickbait_titles
+
+    # @titles = [
+    #   /Won't Believe/i,
+    #   /Secret/i,
+    #   /Top [number]/i,
+    #   /Guess/i
+    # ]
+
+    def clickbait_titles
+      if !/(Won't\sBelieve|Secret|Top\s\d|Guess)/.match(title)
+        errors.add(:title, "Lol this title will get no views, change it asap")
+      end
+    end
 end
