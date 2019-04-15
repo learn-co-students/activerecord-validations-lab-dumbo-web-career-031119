2ddfe4981439c621acef1bdec8715dc84ef5747a
Miguel601 <henriquez.miguel@outlook.com>
2019-04-15 17:11:43 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    
+    # validate :custom_validation
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :title, inclusion: { in: %w("Won't Believe", "Secret", "Top [number]", "Guess")}
+
+
+# private
+
+
+#     def custom_validation
+#         @post = Post.pluck(:title)
+#         if @post
+
+
+
+
 end
